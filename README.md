96e44eb539a4c676bf8740e3f6cfec47192fb40c
rebeca <advertima@advertimas-MacBook-Pro.local>
2020-10-30 08:41:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Data display and control of an IoT sensor network using Arduino
+
+Industrial engineering project that seeks to control a wireless sensor network through Wi-Fi using:
+- Industrial Arduino 101 module
+- PH and conductivity sensors
+
+The network is based on wireless connection.
+A central PC that allows to represent the information of all sensors graphically in real-time with a variable update interval,
+eligible by the user, as well as storing all the values in a database in the cloud
+
+## Project requirements & set up
+
+- Arduino 101 industrial module
+- Linux/MacOS/Windows PC
+- Sensors
+
+## Usage
+Before running the java program, make sure the Arduino and the docker image for the database are running
+### MySQL
+Use `docker-compose up` to start the database in `localhost:8080`
+
---
+++ arduino/arduino-connection.ino
@@ -0,0 +1,68 @@
+#include <SPI.h>
+#include <Console.h>
+#include <Bridge.h>
+#include <BridgeServer.h>
+#include <BridgeClient.h>
+
+int valSensorP;
+int valorSensorC;
+int input;
+float valorP;
+float valorC ;
+int c;
+
+
+BridgeServer server(8080);
+BridgeClient client;
+
+
+float getPH() {
+  // get the reading from the ph sensor
+  valSensorP = analogRead(A0);
+  valorP = valSensorP/36;
+  Console.println(valorP);
+
+  return valorP;
+}
+
+float getC() {
+  // get the reading from the cond sensor
+  //valorSensorC = analogRead(A1);
+ // valorC = valorSensorC*0.008;
+  valorC = random(0,50); // Demo
+  return valorC;
+}
+
+
+void setup() {
+  pinMode(13,OUTPUT);
+  Bridge.begin();
+  server.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  client = server.accept();
+  if (client) {
+    c = client.read();
+    Console.println(c);
+    input = (c*1000);
+    delay(3000);
+    c = client.read();
+    Console.println(c);
+  }
+  if(c == 1){ //When Arduino receives '1' go to connection()
+      connection();
+      }
+ }
+void connection(){ //Connect with Java to send the data
+      getPH();
+      client.write(valorP);
+      client.flush();
+      Console.println("PH data sent");
+      getC();
+      client.write(valorC);
+      client.flush();
+      Console.println("Conductivity data sent");
+      delay(input); //Wait the time you want to get another sample
+}
